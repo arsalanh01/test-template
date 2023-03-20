cf04aa186a72db7ceeb38005827a4f2e64e03465
Arsalan <arsalanh@live.com>
2023-03-20 21:43:50 +1300

    proj init

---
+++ DotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using DotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace DotNetCore.Controllers
+{
+    [Route("api/welcome")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet] 
+
+        public async Task<MessageBox> GetHelloMessage()
+        {
+            return new MessageBox()
+            {
+                Title = "Error",
+                Message = "Hi"
+            };
+        }
+    }
+}
---
+++ DotNetCore/Models/MessageBox.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCore.Models
+{
+    public class MessageBox
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+    }
+}
